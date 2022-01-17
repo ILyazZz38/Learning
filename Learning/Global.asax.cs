5b76ab8902a1fcf7bade4ff0ab59cfb398b2539f
ILyazZz38 <aigozin2001@mail.ru>
2022-01-17 11:21:17 +0300

    Удалил лишнее

--- Learning/Global.asax.cs
+++ Learning/Global.asax.cs
@@ -10,12 +10,31 @@ namespace Learning
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public class FilterConfig
+        {
+            public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+            {
+                filters.Add(new HandleErrorAttribute());
+            }
+        }
+        public class RouteConfig
+        {
+            public static void RegisterRoutes(RouteCollection routes)
+            {
+                routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+                routes.MapRoute(
+                    name: "Default",
+                    url: "{controller}/{action}/{id}",
+                    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
+            }
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
